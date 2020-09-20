28dd4f7113a22aa74b1bd7ad9e6c482a709ed65a
alexkrushkova <alexkrushkova@gmail.com>
2020-09-20 08:33:38 +0200

    01. Cat-Shelter-Workshop solution updated

--- 01. Intro to Node.js/01. Cat-Shelter-Workshop/index.js
+++ 01. Intro to Node.js/01. Cat-Shelter-Workshop/index.js
@@ -1,54 +1,88 @@
 const config = require('./config.json');
-const url = require('url');
 const http = require('http');
-const path = require('path');
-const fx = require('fx');
+const url = require('url');
+const path = require('path'); // за да конструираме целия път
+const fs = require('fs');
+const querystring = require('querystring');
+
+// ----------
 
 const VIEWS_PATH = path.resolve(config.viewsDir);
-const STATIC_FILES_PATH = path.resolve(config.staticFileDir);
+const STATIC_FILES_PATH = path.resolve(config.staticFilesDir);
 
-const routeFileMap = {
-    '/':'/home/index.html',
+// ----------
+
+const routerFileMap = {
+    '/': '/home/index.html',
+    '/addbreed': '/addBreed.html',
+    '/addcat': '/addCat.html',
+};
+
+// -----------
+
+const mimeTypeMap = {
+    'html': 'text/html',
+    'css': 'text/css',
+    'js': 'application/javascript',
+    'json': 'application/json'
 }
 
-function sendFile(res, relativeFilePath){
-    const fullFilePath = path.join(VIEWS_PATH, relativeFilePath);
-    fx.readFile(fullFilePath, function(err, data){
-        const { message }  = err;
-        if (err){
+// -----------
+
+function sendFile(res, fullFilePath) {
+    const fileExt = path.extname(fullFilePath).slice(1);
+    const type = mimeTypeMap[fileExt];
+
+    fs.readFile(fullFilePath, function (err, data) {
+        if (err) {
+            const { message } = err;
             res.writeHead(500, {
-                'Content-Length': Buffer.byteLength(message),
+                'Content-Length': Buffer.byteLength(message), // добре да се слага
                 'Content-Type': 'text/plain'
-            })
-            .end(message);
+            }).end(message);
             return;
-        } 
+        }
+
         res.writeHead(200, {
-            'Content-Length': Buffer.byteLength(data),
-            'Content-Type': 'text/html'
+            'Content-Length': Buffer.byteLength(data), // добре да се слага
+            'Content-Type': type || 'text/plain'
         }).end(data);
-    })
+
+    });
 }
 
-function httpHandler(req, res){
-    const path = url.parse(req.url).pathname;
-    const fileRelativePath = routeFileMap[path];
+// http handler
 
-    if(path.includes(`/${config.staticFileDir}/`)){
-        
+function httpHandler(req, res) {
+    const { pathname, query } = url.parse(req.url, true);
+    const method = req.method.toUpperCase();
+
+    if (method === 'GET') {
+        if (pathname.includes(`/${config.staticFilesDir}/`)) {
+            const fullStaticFilePath = path.resolve(pathname.slice(1));
+            sendFile(res, fullStaticFilePath);
+            return;
+        }
+
+        const fileRelativePath = routerFileMap[pathname];
+
+        if (!fileRelativePath) {
+            const data = "Not Found!"
+            res.writeHead(404, {
+                'Content-Length': Buffer.byteLength(data), // добре да се слага
+                'Content-Type': 'text/plain'
+            }).end(data);
+            return;
+        }
+
+        const fullFilePath = path.join(VIEWS_PATH, fileRelativePath); // обединяване на пътища за целия път
+        sendFile(res, fullFilePath);
+    } else if (method === 'PUT') {
+        res.write(query.breed);
     }
 
-    if(!fileRelativePath ){
-        const data = 'Not Found';
-        res.writeHead(500, {
-            'Content-Length': Buffer.byteLength(data),
-            'Content-Type': 'text/plain'
-        }).end(data);
-        return;
-    }
-    sendFile(res,fileRelativePath);
 }
 
-http.createServer(httpHandler).listen(config.port, function(){
+http.createServer(httpHandler).listen(config.port, function () {
     console.log(`Server is listening on ${config.port}`);
 })
