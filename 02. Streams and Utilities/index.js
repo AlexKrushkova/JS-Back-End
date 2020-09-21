19e8e60c9e7fb8f8e4632102ac401ed023c1ea34
alexkrushkova <alexkrushkova@gmail.com>
2020-09-21 10:42:45 +0200

    Created example of Writable and Readable Streams in index.js

--- 02. Streams and Utilities/index.js
+++ 02. Streams and Utilities/index.js
@@ -2,13 +2,14 @@
 //     console.log(i);
 // }
 
+
+
 // Local Module
 
 // const utils = require('./utils');
 
 // console.log(utils.sum(1, 3));
 // console.log(utils.sub(1, 3));
-
 
 
 
@@ -25,10 +26,11 @@
 
 
 // File System Module
-// FS е Core Module
+// FS is Core Module
 
 // const fs = require('fs');
-// // така автоматично се преобразува до обект от node.js
+// // thats how it automatically get transformed to object in node.js
+
 // const config = require('./config.json');
 // console.log(config.port);
 
@@ -46,48 +48,99 @@
 
 // Creating a simple Node.js web server
 
-const config = require('./config.json');
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
+// const config = require('./config.json');
+// const http = require('http');
+// const fs = require('fs');
+// const url = require('url');
 
-http.createServer((req, res) => {
-    const path = url.parse(req.url).pathname;
-    if (path === '/') {
-        fs.readFile('./text.txt', { encoding: 'utf-8' }, function (err, data) {
-            res.end(data.toUpperCase());
-        });
-    } else if (path === '/test') {
-        res.end('HELLO!');
-    }
-    // res.write('Hello world!');
-    // res.end('Hello World!');
-}).listen(config.port);
-
-
-
-// Request Wrapper Example
-
-const newPort = 3003;
-
-http.createServer((req, res) => {
-    let path = url.parse(req['url']).pathname;
-    if (path === '/') {
-        res.write('Welcome to home page!');
-        res.end();
-    }
-}).listen(newPort);
+// http.createServer((req, res) => {
+//     const path = url.parse(req.url).pathname;
+//     if (path === '/') {
+//         fs.readFile('./text.txt', { encoding: 'utf-8' }, function (err, data) {
+//             res.end(data.toUpperCase());
+//         });
+//     } else if (path === '/test') {
+//         res.end('HELLO!');
+//     }
+//     // res.write('Hello world!');
+//     // res.end('Hello World!');
+// }).listen(config.port);
 
 
 
-// Response Wrapper Example
 
-const newPort2 = 3004;
 
-http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.write('Hello from Node.js');
-    res.end();
-}).listen(newPort2);
+// // Request Wrapper Example
+
+// const newPort = 3003;
+
+// http.createServer((req, res) => {
+//     let path = url.parse(req['url']).pathname;
+//     if (path === '/') {
+//         res.write('Welcome to home page!');
+//         res.end();
+//     }
+// }).listen(newPort);
+
+
+
+
+// // Response Wrapper Example
+// const newPort2 = 3004;
+
+// http.createServer((req, res) => {
+//     res.writeHead(200, {
+//         'Content-Type': 'text/plain'
+//     });
+//     res.write('Hello from Node.js');
+//     res.end();
+// }).listen(newPort2);
+
+
+
+
+
+// //Creating readable stream:
+
+// const stream = require('stream');
+
+// function createReadableStream(data){
+//     let counter = 0;
+//     const rs = stream.Readable({
+//         read(size){
+//             const item = data[counter++] || null;
+//             this.push(item ? Buffer.from(item.toString()) : null);
+//         }
+//     });
+//     return rs;
+// }
+
+
+// // Creating Writable Stream
+
+// function createWritableStream(){
+//     let data;
+
+//     const ws = stream.Writable({
+//         write(chunk, enc, next){
+//             data = !data ? chunk : Buffer.concat([data, chunk]);
+//             next();
+//         },
+//         final(){
+//             console.log(data);
+//         }
+//     });
+//     return ws;
+// }
+
+// const rs = createReadableStream([1,2,3,4,5,6,7]);
+// const ws = createWritableStream();
+
+// ws.on('finish',function(){
+//     console.log(data);
+// });
+
+// //Sending the data from the readable stream to the writable.. 
+// rs.pipe(ws);
+
+
