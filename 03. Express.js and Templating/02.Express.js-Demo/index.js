8708f5ca2658ab8e639b0cd137c8982533e3c530
alexkrushkova <alexkrushkova@gmail.com>
2020-09-25 20:24:18 +0200

    Demo Express.js and Handlebars updated

--- 03. Express.js and Templating/02.Express.js-Demo/index.js
+++ 03. Express.js and Templating/02.Express.js-Demo/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const handlebars = require('express-handlebars');
 
 const app = express();
 const jsonBodyParser = express.json();
@@ -13,11 +14,46 @@ const jsonBodyParser = express.json();
 
 // app.use(logRequestedDate);
 app.use(jsonBodyParser);
+const urlEncodedParserBody = express.urlencoded({ extended : true})
+
+
+const users = [
+    { name: 'Maria Petrova', age: 20},
+    { name: 'Jordan Kirov', age: 16}
+];
+
+
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
 app.use('/public', express.static('./static'));
 
+// app.get('/', function(req, res){
+//     res.sendFile(path.resolve('./index.html'));
+// });
+
 app.get('/', function(req, res){
-    res.sendFile(path.resolve('./index.html'));
-});
+    res.render('home', {users});
+    });
+
+app.get('/about', function(req, res){
+        res.render('about');
+    });
+
+app.get('/user/:idx', function (req, res){
+    const selectedUser = users[req.params.idx]
+    res.render('home', { users, selectedUser});
+})
+
+app.post('/user', urlEncodedParserBody ,function(req, res){
+    const { name, age } = req.body;
+    users.push({name, age: +age});
+    res.redirect('/');
+    
+})
+    
 
 app.post('/',  function(req, res){
     console.log(req.body)
