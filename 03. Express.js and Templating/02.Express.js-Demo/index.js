16ac7ddd644e849b5c39d7e4d3bbc213bfa037f2
alexkrushkova <alexkrushkova@gmail.com>
2020-10-01 08:54:11 +0200

    Demo Express.js updated

--- 03. Express.js and Templating/02.Express.js-Demo/index.js
+++ 03. Express.js and Templating/02.Express.js-Demo/index.js
@@ -3,63 +3,55 @@ const path = require('path');
 const handlebars = require('express-handlebars');
 
 const app = express();
+
 const jsonBodyParser = express.json();
-
-// function logRequestedDate(req, res, next){
-//     console.log(new Date());
-//     setTimeout(function (){
-//         next();
-//     }, 3000)
-// }
-
-// app.use(logRequestedDate);
-app.use(jsonBodyParser);
-const urlEncodedParserBody = express.urlencoded({ extended : true})
-
+const urlencodedBodyParser = express.urlencoded({ extended: true });
 
 const users = [
-    { name: 'Maria Petrova', age: 20},
-    { name: 'Jordan Kirov', age: 16}
+  { name: 'Ivan', age: 20 },
+  { name: 'Stefan', age: 30 }
 ];
 
-
-app.engine('hbs', handlebars({
-    extname: '.hbs'
+app.engine('.hbs', handlebars({
+  extname: '.hbs'
 }));
+app.set('view engine', '.hbs');
 
-app.set('view engine', 'hbs');
+app.use(jsonBodyParser);
+app.use(urlencodedBodyParser);
+
 app.use('/public', express.static('./static'));
 
-// app.get('/', function(req, res){
-//     res.sendFile(path.resolve('./index.html'));
-// });
-
-app.get('/', function(req, res){
-    res.render('home', {users});
-    });
-
-app.get('/about', function(req, res){
-        res.render('about');
-    });
-
-app.get('/user/:idx', function (req, res){
-    const selectedUser = users[req.params.idx]
-    res.render('home', { users, selectedUser});
-})
-
-app.post('/user', urlEncodedParserBody ,function(req, res){
-    const { name, age } = req.body;
-    users.push({name, age: +age});
-    res.redirect('/');
-    
-})
-    
-
-app.post('/',  function(req, res){
-    console.log(req.body)
-    res.send(req.body);
+app.get('/', function (req, res) {
+  res.render('home', { users });
 });
 
-app.listen(3000, function(){
-    console.log('App is listening on port 3000');
+app.get('/user/:idx', function (req, res) {
+  const selectedUser = users[req.params.idx];
+  res.render('home', { users, selectedUser, selectedUserIndex: req.params.idx });
+});
+
+app.post('/user/:idx', function (req, res) {
+  const { name, age } = req.body;
+  users[req.params.idx] = { name, age: +age };
+  res.redirect('/');
+});
+
+app.post('/user', function (req, res) {
+  const { name, age } = req.body;
+  users.push({ name, age: +age });
+  res.redirect('/');
+});
+
+app.get('/about', function (req, res) {
+  res.render('about');
+});
+
+app.post('/', function (req, res) {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.listen(3000, function () {
+  console.log('App is listening on :3000');
 });
